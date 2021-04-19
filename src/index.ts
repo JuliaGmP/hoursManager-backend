3b12b770e582160cd46e4063dffc8ef5dc83467e
JuliaGmP <gmezpozojuli95@gmail.com>
2021-04-19 16:02:05 +0200

    feat: Added loopback files

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {HoursManagerBackendApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {HoursManagerBackendApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new HoursManagerBackendApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
